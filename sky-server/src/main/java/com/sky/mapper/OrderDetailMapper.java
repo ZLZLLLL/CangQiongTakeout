412c22c1ca225e8f3b945403fa01e58048046809
ZLL <3080875439@qq.com>
2025-04-26 17:01:56 +0800

    完成下单与微信支付功能

---
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -0,0 +1,30 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mapper
+public interface OrderDetailMapper {
+
+    /**
+     * 批量插入订单详情数据
+     *
+     * @param orderDetailList
+     */
+    void insertBatch(ArrayList<OrderDetail> orderDetailList);
+
+
+    /**
+     * 根据订单id查询菜品数据
+     *
+     * @param orderId
+     * @return
+     */
+    @Select("select * from order_detail where order_id=#{orderId}")
+    List<OrderDetail> getByOrderId(Long orderId);
+}
